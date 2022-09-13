25e88d09e40461480a266b3fdea97267cc9fd506
Sidney Kaguli <developer.sid@outlook.com>
2022-09-13 21:00:23 +0300

    insert 7 more records

--- data.sql
+++ data.sql
@@ -6,3 +6,14 @@ VALUES
 	('Gabumon', TO_DATE('11/15/2020', 'MM/DD/YYYY'), 2, true, 8),
 	('Pikachu', TO_DATE('01/07/2021', 'MM/DD/YYYY'), 1, false, 15.04),
 	('Devimon', TO_DATE('05/12/2017', 'MM/DD/YYYY'), 5, true, 11);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES
+	('Charmander', 'Feb 8, 2020', 0, false, 11 ),
+  ('Plantmon', 'Nov 15, 2021', 2, true, 5.7 ),
+  ('Squirtle', 'Apr 2, 1993', 3, false, 12.13 ),
+  ('Angemon', 'Jun 12, 2005', 1, true, 45 ),
+  ('Boarmon', 'Jun 7, 2005', 7, true, 20.4 ),
+  ('Blossom', 'Oct 13, 1998', 3, true, 17 ),
+  ('Ditto', 'May 14, 2022', 4, true, 22);
+
