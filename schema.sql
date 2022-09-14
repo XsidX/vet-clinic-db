d8f3afb551b2a1c18323ed828b59c75a3fa27d69
Sidney Kaguli <developer.sid@outlook.com>
2022-09-14 13:53:31 +0300

    modiify animals tbl, create owners & species tbls

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 
+-- create animals table
 CREATE TABLE animals (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(50),
@@ -7,8 +8,36 @@ CREATE TABLE animals (
 	escape_attempts INTEGER,
 	neutered BOOLEAN,
 	weight_kg DECIMAL
-)
+);
 
+-- add species column on animals table
 ALTER TABLE animals
 ADD species VARCHAR(50)
 
+-- remove species column from animals table
+ALTER TABLE animals
+DROP COLUMN species
+
+-- add species_id column on animals table as foreign key
+ALTER TABLE animals
+ADD species_id INTEGER REFERENCES species(id)
+
+-- add owner_id column on animals table as foreign key
+ALTER TABLE animals
+ADD owner_id INTEGER REFERENCES owners(id)
+
+
+
+
+-- create owners table
+CREATE TABLE owners (
+	id SERIAL PRIMARY KEY,
+  full_name VARCHAR(50),
+  age INTEGER
+);
+
+-- create species table
+CREATE TABLE species (
+	id SERIAL PRIMARY KEY,
+  name VARCHAR(50)
+);
