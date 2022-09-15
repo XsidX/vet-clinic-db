2becd352ca6a086ddca242c4b05d1f6c71edaadf
Sidney Kaguli <developer.sid@outlook.com>
2022-09-15 11:35:00 +0300

    insert vets, specializations and visits data

--- data.sql
+++ data.sql
@@ -73,6 +73,73 @@ SET owner_id = 5
 WHERE name = 'Angemon' OR name = 'Boarmon';
 
 
+-- insert into vets table
+INSERT INTO vets (name, age, date_of_graduation)
+VALUES
+  ('William Tatcher', 45, 'Apr 23, 2000'),
+  ('Maisy Smith', 26, 'Jan 17, 2019'),
+  ('Stephanie Mendez', 64, 'May 4, 1981'),
+  ('Jack Harkness', 38, 'Jun 8, 2008');
+
+-- insert into specializations table
+-- Vet William Tatcher is specialized in Pokemon.
+-- Vet Stephanie Mendez is specialized in Digimon and Pokemon.
+-- Vet Jack Harkness is specialized in Digimon.
+
+INSERT INTO specializations (vet_id, species_id)
+VALUES
+  (1, 1),
+  (3, 1),
+  (3, 2),
+  (4, 2);
+
+-- insert into visits table
+
+-- Agumon visited William Tatcher on May 24th, 2020.
+-- Agumon visited Stephanie Mendez on Jul 22th, 2020.
+-- Gabumon visited Jack Harkness on Feb 2nd, 2021.
+-- Pikachu visited Maisy Smith on Jan 5th, 2020.
+-- Pikachu visited Maisy Smith on Mar 8th, 2020.
+-- Pikachu visited Maisy Smith on May 14th, 2020.
+-- Devimon visited Stephanie Mendez on May 4th, 2021.
+-- Charmander visited Jack Harkness on Feb 24th, 2021.
+-- Plantmon visited Maisy Smith on Dec 21st, 2019.
+-- Plantmon visited William Tatcher on Aug 10th, 2020.
+-- Plantmon visited Maisy Smith on Apr 7th, 2021.
+-- Squirtle visited Stephanie Mendez on Sep 29th, 2019.
+-- Angemon visited Jack Harkness on Oct 3rd, 2020.
+-- Angemon visited Jack Harkness on Nov 4th, 2020.
+-- Boarmon visited Maisy Smith on Jan 24th, 2019.
+-- Boarmon visited Maisy Smith on May 15th, 2019.
+-- Boarmon visited Maisy Smith on Feb 27th, 2020.
+-- Boarmon visited Maisy Smith on Aug 3rd, 2020.
+-- Blossom visited Stephanie Mendez on May 24th, 2020.
+-- Blossom visited William Tatcher on Jan 11th, 2021.
+
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+VALUES
+  (1, 1, 'May 24, 2020'),
+  (1, 3, 'Jul 22, 2020'),
+  (2, 4, 'Feb 2, 2021'),
+  (3, 2, 'Jan 5, 2020'),
+  (3, 2, 'Mar 8, 2020'),
+  (3, 2, 'May 14, 2020'),
+  (4, 3, 'May 4, 2021'),
+  (5, 4, 'Feb 24, 2021'),
+  (6, 2, 'Dec 21, 2019'),
+  (6, 1, 'Aug 10, 2020'),
+  (6, 2, 'Apr 7, 2021'),
+  (7, 3, 'Sep 29, 2019'),
+  (8, 4, 'Oct 3, 2020'),
+  (8, 4, 'Nov 4, 2020'),
+  (9, 2, 'Jan 24, 2019'),
+  (9, 2, 'May 15, 2019'),
+  (9, 2, 'Feb 27, 2020'),
+  (9, 2, 'Aug 3, 2020'),
+  (10, 3, 'May 24, 2020'),
+  (10, 1, 'Jan 11, 2021');
+
+
 
 
 
